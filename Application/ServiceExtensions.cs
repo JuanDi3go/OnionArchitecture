4cbb8992913cb7b59767d487d581d0b74639c65e
Diego <juandiegoaguilarok@gmail.com>
2023-02-26 12:18:50 -0500

    Hasta el video 10 sin errores

---
+++ Application/ServiceExtensions.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Application
+{
+    public static class ServiceExtensions
+    {
+        public static void AddApplicationLayer(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            // registra automaticamente los mappeos que se hagan en esta biblioteca de clase
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+        }
+    }
+}
